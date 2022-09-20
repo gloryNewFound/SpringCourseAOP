9bbfd59b3e99fbbaddb3a8c7ea00f09d7da0b042
dvn <glorynewfound@yandex.ru>
2022-09-20 23:05:00 +0300

    StudyProject

---
+++ src/aop/MyConfig.java
@@ -0,0 +1,10 @@
+package aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("aop")
+public class MyConfig {
+}
